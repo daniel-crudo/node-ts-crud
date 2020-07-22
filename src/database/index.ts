df75ade13f35bcf4fb498eaecd65c6a2ad487933
Daniel Crudo <daniel.crudo@hvarconsulting.com.br>
2020-07-21 23:01:22 -0300

    resolve port localhost

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,6 @@
 import mongoose from 'mongoose';
 
-mongoose.connect('mongodb://localhost:27017/V12', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost:27017/api', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
