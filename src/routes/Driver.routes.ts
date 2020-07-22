c59362d9deab4cca73b7cb52ee0d01caf0d303c4
Daniel Crudo <daniel.crudo@hvarconsulting.com.br>
2020-07-21 23:02:07 -0300

    modified string

--- src/routes/Driver.routes.ts
+++ src/routes/Driver.routes.ts
@@ -1,7 +1,10 @@
-import { Router } from 'express';
+import { Router, response } from 'express';
 
 const Driver = Router()
 
+Driver.get('/', (request,response) => {
+  response.json("Bateu!")
+})
 
 
 export default Driver
