b47cf2003adb91de453c98a4455de752156f6ee2
Daniel Crudo <daniel.crudo@hvarconsulting.com.br>
2020-07-21 21:42:15 -0300

    server configuration and connect to database

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
 import express from 'express';
 import routes from './routes/index';
+import './database/index';
+import cors from 'cors';
+import requireDir from 'require-dir';
 
 const app = express();
 
+// cors controla o acesso das requisições.
+app.use(cors());
+
 app.use(express.json());
+app.use(routes);
+
+// importa automaticamente tudo que estiver dentro da pasta models.
+requireDir('./models');
 
 app.listen(3333, () => console.log('Welcome V12 you application ir running on port 3333'))
 
